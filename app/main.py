e7cbc6115b08fdc2e7149f083d4905733bb16b87
Lucas Battisti <lucas.sala.battisti.pessoal@gmail.com>
2025-07-04 16:15:05 -0300

    main

--- app/main.py
+++ app/main.py
@@ -11,7 +11,6 @@ from skills import evaluate_skill_requirements, compute_skills
 from questions import generate_interview_questions
 
 
-
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
@@ -24,8 +23,7 @@ async def analisar_curriculo(
 ):
     pontos_fortes = []
     pontos_fracos = []
-    
-    
+
     profile_json = create_profile(arquivo_pdf)
     description_dict = create_description(contexto_vaga)
 
@@ -38,49 +36,72 @@ async def analisar_curriculo(
     language_requirements = description_dict["languages"]
 
     if len(education_requirements) != 0:
-        education_info = evaluate_education_requirements(education_requirements, profile_json)
+        education_info = evaluate_education_requirements(
+            education_requirements, profile_json
+        )
         education_score, fortes, fracos = compute_education(education_info)
         pontos_fortes.extend(fortes)
         pontos_fracos.extend(fracos)
-    
-    else: education_score = 0
+
+    else:
+        education_score = 0
 
     if len(experience_requirements) != 0:
-        experience_info = evaluate_experience_requirements(experience_requirements, profile_json)
+        experience_info = evaluate_experience_requirements(
+            experience_requirements, profile_json
+        )
         experience_score, fortes, fracos = compute_experience(experience_info)
         pontos_fortes.extend(fortes)
         pontos_fracos.extend(fracos)
 
-    else: experience_score = 0
+    else:
+        experience_score = 0
 
-    skills_info = evaluate_skill_requirements(hard_skills_requirements,
-                                        soft_skills_requirements,
-                                        language_requirements,
-                                        profile_json)
-    
-    hard_skill_score, soft_skill_score, language_score, fortes, fracos, perguntas_lista = compute_skills(skills_info)
+    skills_info = evaluate_skill_requirements(
+        hard_skills_requirements,
+        soft_skills_requirements,
+        language_requirements,
+        profile_json,
+    )
+
+    (
+        hard_skill_score,
+        soft_skill_score,
+        language_score,
+        fortes,
+        fracos,
+        perguntas_lista,
+    ) = compute_skills(skills_info)
     pontos_fortes.extend(fortes)
     pontos_fracos.extend(fracos)
     perguntas.extend(perguntas_lista)
 
-    geral_score = 0.4*experience_score + 0.25*hard_skill_score + 0.2*education_score + 0.1*soft_skill_score + 0.05*language_score
+    geral_score = (
+        0.4 * experience_score
+        + 0.25 * hard_skill_score
+        + 0.2 * education_score
+        + 0.1 * soft_skill_score
+        + 0.05 * language_score
+    )
 
     return JSONResponse(
-    {
-        "Escores": {
-            "Geral": round(geral_score, 2),
-            "Educação": round(education_score, 2) if len(education_requirements) != 0 else 0,
-            "Experiência": round(experience_score, 2) if len(experience_requirements) != 0 else 0,
-            "Hard Skills": round(hard_skill_score, 2),
-            "Soft Skills": round(soft_skill_score, 2),
-            "Idiomas": round(language_score, 2),
-        },
-        "Pontos fortes": pontos_fortes,
-        "Pontos fracos": pontos_fracos,
-        "Perguntas": perguntas,
-    }
-)
-
-
-
-
+        {
+            "Escores": {
+                "Geral": round(geral_score, 2),
+                "Educação": (
+                    round(education_score, 2) if len(education_requirements) != 0 else 0
+                ),
+                "Experiência": (
+                    round(experience_score, 2)
+                    if len(experience_requirements) != 0
+                    else 0
+                ),
+                "Hard Skills": round(hard_skill_score, 2),
+                "Soft Skills": round(soft_skill_score, 2),
+                "Idiomas": round(language_score, 2),
+            },
+            "Pontos fortes": pontos_fortes,
+            "Pontos fracos": pontos_fracos,
+            "Perguntas": perguntas,
+        }
+    )
